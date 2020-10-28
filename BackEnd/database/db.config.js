ee0a67889e5a4481fdcec999469af84732745219
odrugees <odrugees@poligran.edu.co>
2020-10-28 17:24:41 -0500

    Servidor Remoto

--- BackEnd/database/db.config.js
+++ BackEnd/database/db.config.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-  HOTS: "localhost",
-  USER: "root",
-  PASSWORD: "mysql2020",
-  DB: "coradb",
+  HOTS: 'vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  USER: 'uz6i7f3oaf38hyon',
+  PASSWORD: 'q9c1q5ybjgfsb4ql',
+  DB: 'i8fusb36ynxj9m7z',
   dialect: "mysql",
   pool: {
     max: 5,
